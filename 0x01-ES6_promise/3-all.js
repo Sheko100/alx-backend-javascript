e87715a6da4ddafffe3cbc6b4633a7e8706482f3
Shaker Sharabi <sheko100@icloud.com>
2024-05-28 20:49:08 +0300

    Add 3-all.js file

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,15 @@
+import { createUser, uploadPhoto } from './utils';
+
+export default function handleProfileSignup() {
+  const createPrms = createUser();
+  const upldPrms = uploadPhoto();
+
+  Promise.all([createPrms, upldPrms])
+    .then((response) => {
+      const name = response[0];
+      const resObj = response[1];
+      console.log(`${resObj.body} ${name.firstName} ${name.lastName}`);
+    }, () => {
+      console.log('Signup system offline');
+    });
+}
