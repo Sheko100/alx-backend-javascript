1863f2e6dcdf7f1702802f9248d48a7200c55f9f
Shaker Sharabi <sheko100@icloud.com>
2024-05-28 13:06:55 +0300

    Add 1-block-scoped.js file

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -3,8 +3,9 @@ export default function taskBlock(trueOrFalse) {
   const task2 = true;
 
   if (trueOrFalse) {
-    const task = true;
-    const task2 = false;
+    const task1 = true;
+    const task3 = false;
+    console.log(task1, task3);
   }
 
   return [task, task2];
