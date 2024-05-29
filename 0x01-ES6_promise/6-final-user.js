94d9b75ab1e7f563a12cce4cf2d1f5d550556422
Shaker Sharabi <sheko100@icloud.com>
2024-05-29 18:16:46 +0300

    Add 6-final-user.js file

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -4,7 +4,5 @@ import uploadPhoto from './5-photo-reject';
 export default function handleProfileSignup(firstName, lastName, fileName) {
   const promises = [signUpUser(firstName, lastName), uploadPhoto(fileName)];
 
-  let results = Promise.allSettled(promises);
-
-  return results;
+  return Promise.allSettled(promises);
 }
