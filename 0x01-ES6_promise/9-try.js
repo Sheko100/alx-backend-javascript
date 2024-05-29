bfc61655c6127059744758b8f8af911ae73be492
Shaker Sharabi <sheko100@icloud.com>
2024-05-29 20:28:24 +0300

    Add 9-try.js file

---
+++ 0x01-ES6_promise/9-try.js
@@ -0,0 +1,15 @@
+export default function guardrail(mathFunction) {
+  const queue = [];
+  let res = 0;
+
+  try {
+    res = mathFunction();
+    queue.push(res);
+  } catch (err) {
+    queue.push(err);
+  } finally {
+    queue.push('Guardrail was processed');
+  }
+
+  return queue;
+}
