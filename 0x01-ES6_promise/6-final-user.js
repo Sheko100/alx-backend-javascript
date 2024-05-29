2ec1deccf2253247f8e99225302614a7e4254095
Shaker Sharabi <sheko100@icloud.com>
2024-05-30 00:46:29 +0300

    Add 6-final-user.js file

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,9 +1,21 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default async function handleProfileSignup(firstName, lastName, fileName) {
+export default function handleProfileSignup(firstName, lastName, fileName) {
   const promises = [signUpUser(firstName, lastName), uploadPhoto(fileName)];
 
-  const results = await Promise.allSettled(promises);
-  return results;
+  return Promise.allSettled(promises).then((results) => {
+    const resArr = [];
+    results.forEach((result) => {
+      const res = { status: null, value: null };
+      res.status = result.status;
+      if ('reason' in result) {
+        res.value = result.reason;
+      } else {
+        res.value = result.value;
+      }
+      resArr.push(res);
+    });
+    return resArr;
+  });
 }
