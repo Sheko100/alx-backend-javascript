2b6de9c9538614547e68ce296e7cf7eb71df378d
Shaker Sharabi <sheko100@icloud.com>
2024-05-29 23:17:31 +0300

    Add 2-then.js file

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,6 +1,9 @@
 export default function handleResponseFromAPI(promise) {
   promise.then(() => {
-    console.log('Got a response from the API');
     return { status: 200, body: 'success' };
-  }, () => Error());
+  }).catch(() => {
+    return new Error();
+  }).finally(() => {
+    console.log('Got a response from the API');
+  });
 }
