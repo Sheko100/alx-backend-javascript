50a7cccde1f40a5601d83bd6a9753a17c1c8e94e
Shaker Sharabi <sheko100@icloud.com>
2024-06-04 00:50:48 +0300

    Add 1-get_list_student_ids.js file

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,9 +1,9 @@
 export default function getListStudentIds(stdntsArr) {
   let ids = [];
 
-  /*if (Array.isArray(stdntsArr)) {
+ // if (Array.isArray(stdntsArr)) {
     ids = stdntsArr.map((stdnt) => stdnt.id);
-  }*/
+  //}
 
   return ids;
 }
