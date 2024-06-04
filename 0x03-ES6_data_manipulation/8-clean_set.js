9b62d959d09a4bba218159a52e9b19e97d43dbfe
Shaker Sharabi <sheko100@icloud.com>
2024-06-05 02:56:29 +0300

    Add 8-clean_set.js file

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -2,7 +2,7 @@ export default function cleanSet(set, startString) {
   let str = '';
   const strArr = [];
 
-  if (startString.length > 0) {
+  if (startString.length > 0 && set.size > 0) {
     set.forEach((val) => {
       if (val.startsWith(startString)) {
         strArr.push(val.slice(startString.length));
