c4ee48a15af81ca7c26326309077902388aea667
Shaker Sharabi <sheko100@icloud.com>
2024-06-04 08:14:49 +0300

    Add 0-get_list_students.js file

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,9 +1,16 @@
-export default function getListStudents() {
-  const arr = Array();
+/* export default function getListStudents() {
+  const arr = [];
   arr.push(
     { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
     { id: 2, firstName: 'James', location: 'Columbia' },
     { id: 3, firstName: 'Serena', location: 'San Francisco' },
   );
   return arr;
+} */
+export default function getListStudents() {
+  return [
+    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+    { id: 2, firstName: 'James', location: 'Columbia' },
+    { id: 5, firstName: 'Serena', location: 'San Francisco' },
+  ];
 }
