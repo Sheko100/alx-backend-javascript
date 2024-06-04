5d710ff70a47f62bf268fae114852b55228efe49
Shaker Sharabi <sheko100@icloud.com>
2024-06-05 02:50:14 +0300

    Add 8-clean_set.js file

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -2,15 +2,13 @@ export default function cleanSet(set, startString) {
   let str = '';
   const strArr = [];
 
-  /*if (startString === '') {
-    return str;
-  }*/
-
-  set.forEach((val) => {
-    if (val.startsWith(startString)) {
-      strArr.push(val.slice(startString.length));
-    }
-  });
-  str = strArr.join('-');
+  if (startString.length > 0) {
+    set.forEach((val) => {
+      if (val.startsWith(startString)) {
+        strArr.push(val.slice(startString.length));
+      }
+    });
+    str = strArr.join('-');
+  }
   return str;
 }
