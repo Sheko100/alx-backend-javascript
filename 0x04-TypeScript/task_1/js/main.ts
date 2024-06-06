20c63c1ad5bc3108a61068884bdc3bc65a2f43bc
Shaker Sharabi <sheko100@icloud.com>
2024-06-06 04:31:53 +0300

    Add Student class and interface

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -18,6 +18,35 @@ const printTeacher: printTeacherFunction = function (fName, lName) {
   console.log(`${fName[0]}, ${lName}`);
 }
 
+interface StudentInterface {
+  firstName: string,
+  lastName: string,
+  workOnHomework(): string,
+  displayName(): string,
+}
+
+interface StudentConstructor {
+  (firstName: string, lastName: string): StudentInterface,
+}
+
+class StudentClass implements StudentInterface {
+  firstName: string;
+  lastName: string;
+
+  constructor(firstName: string, lastName: string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  workOnHomework(): string {
+    return 'Currently working';
+  }
+
+  displayName(): string {
+    return this.firstName;
+  }
+}
+
 const director1: Directors = {
   firstName: 'John',
   lastName: 'Doe',
